a6ef3b2155ad7ba9b7596c204929383df6609ec1
Glitch (personality-quiz-starter-code) <none>
2020-07-14 05:08:00 +0000

    📌😂 Checkpoint ./index.html:18578901/12 ./script.js:18578901/621

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //This is where you will define the variables you will be using in your project.
 //#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
-var elenaCount = 0;
-var damonScore = 0;
-var stefenScore = 0;
+var edwardScore = 0;
+var bellaScore = 0;
+var jacobScore = 0;
 
 
 
@@ -12,7 +12,22 @@ var stefenScore = 0;
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
+var result = document.getElementById("result");
+
 var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1q3");
+var q1a4 = document.getElementById("q1a4");
+
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2q3");
+var q2a4 = document.getElementById("q2a4");
+
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3q3");
+var q3a4 = document.getElementById("q3a4");
 
 
 
